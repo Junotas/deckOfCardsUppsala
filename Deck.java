22de3cc5aee61b2279d025df0e38591d5745ef6d
Ludvig <l.fendert@gmail.com>
2025-04-11 15:08:12 +0200

    updated deck

--- Deck.java
+++ Deck.java
@@ -1,32 +1,35 @@
 import java.util.*;
 
 public class Deck {
-    
+
     private static final String back = "img/b1fv.gif";
     private static final String[] suites = {"s", "h", "d", "c"};
     private static final String[] ranks =
-    {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k"};
-    
+            {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k"};
+
     private ArrayList<Card> cardSet = new ArrayList<Card>();
-    private Random randomGenerator=new Random();
-    
-    public Deck () {
-        for (int s = 0; s<4; s++) {
-	    for (int r = 0; r<13; r++) {
-		String front = "img/"+suites[s]+ranks[r]+".gif";
-		Card c = new Card(s, r, front, back);
-		cardSet.add(c);
-	    }
-	}
+    private Random randomGenerator = new Random();
+
+    public Deck() {
+        // Skapa två kortlekar, dvs 104 kort
+        for (int deck = 0; deck < 2; deck++) {
+            for (int s = 0; s < 4; s++) {
+                for (int r = 0; r < 13; r++) {
+                    String front = "img/" + suites[s] + ranks[r] + ".gif";
+                    Card c = new Card(s, r, front, back);
+                    cardSet.add(c);
+                }
+            }
+        }
     }
 
     public void shuffle() {
-        for (int i = 0; i<52; i++) {
-            int j = i+randomGenerator.nextInt(52-i);
+        for (int i = 0; i < cardSet.size(); i++) {
+            int j = i + randomGenerator.nextInt(cardSet.size() - i);
             swap(i, j);
         }
     }
-    
+
     private void swap(int i, int j) {
         Card t = cardSet.get(i);
         cardSet.set(i, cardSet.get(j));
@@ -34,7 +37,6 @@ public class Deck {
     }
 
     public ArrayList<Card> cardSet() {
-        return new ArrayList<Card> (cardSet); //why?
+        return new ArrayList<Card>(cardSet);
     }
 }
-
